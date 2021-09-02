bf6bb6611a7bd598a694c23936b2328aeb8b126b
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-09-02 16:14:40 -0400

    added element variables and updated event listener to add input values to myLeads array

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-let myLeads = [];
+let myLeads = ["www.gamespot.com", "www.gamefaqs.com", "www.gameshark.com"];
 
-const inputEl = document.createElementById('input-el');
+const inputEl = document.getElementById('input-el');
 
 const inputBtn = document.getElementById('input-btn');
 
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from event listener")
-})
+const ulEl = document.getElementById('ul-el');
 
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value.length === 0) {
+        throw new Error('Required')
+    } else {
+        myLeads.push(inputEl.value);
+        console.log(inputEl.value + " Added!");
+    }
+})
