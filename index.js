1273103bf3369ba73e45e43597676e6db3461c1f
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-09-02 15:45:11 -0400

    added variable for leads array, variables for input and button elements, and event listener for button

---
+++ index.js
@@ -0,0 +1,10 @@
+let myLeads = [];
+
+const inputEl = document.createElementById('input-el');
+
+const inputBtn = document.getElementById('input-btn');
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from event listener")
+})
+
