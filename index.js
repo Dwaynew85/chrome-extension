1d270abb3c10040b277a45b73ab5f3e148007f19
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-09-03 14:03:26 -0400

    leads save to local storage and are pulled from local storage to save leads list despite reload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.gamefaqs.com", "www.marvel.com", "www.imdb.com"];
+let myLeads = [];
 
 const inputEl = document.getElementById('input-el');
 
@@ -6,16 +6,23 @@ const inputBtn = document.getElementById('input-btn');
 
 const ulEl = document.getElementById('ul-el');
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     if (inputEl.value.length === 0) {
         throw new Error('Required')
     } else {
         myLeads.push(inputEl.value);
         inputEl.value = "";
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
         renderLeads();
     }
 })
-
 
 function renderLeads() {
     let listItems = ""
